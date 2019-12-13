e0e70846ab32de34af857b669160cd50fdd5ec65
nicolo <nicolo.ghielmetti@gmail.com>
2019-12-13 23:45:14 +0100

    Implemented the initialization part of the driver for the lps22hb. It seems it works only for reading values from i2c.

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,7 @@
+For debugging using NetBeansIDE
 
+- install gdbserver plugin for NetBeansIDE
+- paste the following configuration file (https://github.com/arduino-org/OpenOCD/blob/master/tcl/board/st_nucleo_f401re.cfg) 
+  at the following location -> miosix-kernel/miosix/arch/cortexM4_stm32f4/stm32f401re_nucleo/
+- run -> openocd -f miosix-kernel/miosix/arch/cortexM4_stm32f4/stm32f401re_nucleo/stm32f401re_nucleo.cfg
+- within NetBeansIDE -> Debug -> Attach Debug : select "gdbserver" and as target "ext :3333" -> OK
--- main.cpp
+++ main.cpp
@@ -1,30 +1,61 @@
+//default baudrate 19200
+
 
 #include <cstdio>
+#include <iostream>
 #include "miosix.h"
 #include "lstm.h"
-#include "util/software_i2c.h"
-
+#include "IKS01A2_classes/lps22hb.h"
 
 using namespace std;
 using namespace miosix;
 
+void initRCC()
+{
+    //enable RCC
+    FastInterruptDisableLock a;
+    RCC->AHB1ENR |= RCC_AHB1ENR_CRCEN;
+    CRC->CR = CRC_CR_RESET;
+}
+
+
 int main()
 {
-    {
-        FastInterruptDisableLock a;
-        RCC->AHB1ENR |= RCC_AHB1ENR_CRCEN;
-        CRC->CR = CRC_CR_RESET;
-    }
     
-    ai_handle* network;
+    initRCC();
+    //NN variables declaration and initialization
+    /*ai_handle* network = AI_HANDLE_NULL;
     ai_buffer* config;
     ai_buffer* input;
     ai_buffer* output;
-    ai_network_params* params;
+    ai_network_params* params;*/
     
-    ai_error err = ai_lstm_create(network, config);
+    //GPIO for I2C1 declaration
+    typedef Gpio<GPIOB_BASE,8>  scl;
+    typedef Gpio<GPIOB_BASE,9>  sda;
+    typedef Gpio<GPIOA_BASE,5> led;
+    typedef lps22hb<sda,scl> pressure_sensor;
+    
+    
+    //NN variables initialization
+    /*ai_error err = ai_lstm_create(network, config);
     ai_bool bl = ai_lstm_init(network, params);
     ai_i32 i32 = ai_lstm_run(network, input, output);
-    ai_lstm_destroy(network);
+    ai_lstm_destroy(network);*/
     
+    pressure_sensor::init();
+    
+    return 0;
  }
+
+/*
+    typedef Gpio<GPIOA_BASE,5> led;
+
+    for(;;)
+    {
+        led::high();
+        Thread::sleep(1000);
+        led::low();
+        Thread::sleep(1000);
+    }
+ */
