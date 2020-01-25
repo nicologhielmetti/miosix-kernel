a73ac741f9f6dde774cc29ef0a4a395bb0a266c0
francescoalongi <alongi.francesco96@gmail.com>
2020-01-25 20:57:15 +0100

    modified a testing script

--- Testing_scripts/read_predictions.py
+++ Testing_scripts/read_predictions.py
@@ -16,26 +16,37 @@ baudrate = 19200
 ser = serial.Serial(port=serial_port, baudrate=baudrate)  # open serial port
 print(ser.name)
 
-data_output = open("Predictions.csv", "w+")
+data_output_pred = open("Predictions.csv", "w+")
+data_output_mean = open("Ground_truth.csv", "w+")
 
 #write the header
-data_output.write('datetime,prediction\n')
+data_output_pred.write('datetime,prediction\n')
+data_output_mean.write('datetime,mean\n')
 
 # enter here the number of prediction to record
 pr_n = 3
 
 print('Predictions recording started...')
 # this is the string that is needed to wipe out in order to obtain the prediction
-startString = 'Prediction result: '
+startStringPrediction = 'Prediction result: '
+startStringMean = 'Mean: '
 i = 0
 while i < pr_n:
     reading = ser.readline().decode('utf-8')
-    if reading.startswith(startString):
+    if reading.startswith(startStringPrediction):
         now = datetime.now()
-        string_to_write = str(now) + ',' + str(re.sub(startString + '|\n', '', reading))
-        data_output.write(string_to_write)
-        print('Recorded: ' + string_to_write)
+        string_to_write = str(now) + ',' + str(re.sub(startStringPrediction + '|\n', '', reading))
+        data_output_pred.write(string_to_write)
+        data_output_pred.flush()
+        print('Recorded prediction: ' + string_to_write)
         i += 1
+    if reading.startswith(startStringMean):
+        now = datetime.now()
+        string_to_write = str(now) + ',' + str(re.sub(startStringMean + '|\n', '', reading))
+        data_output_mean.write(string_to_write)
+        data_output_mean.flush()
+        print('Recorded mean: ' + string_to_write)
 
 print('Predictions recording ended.')
-data_output.close()
+data_output_pred.close()
+data_output_mean.close()
